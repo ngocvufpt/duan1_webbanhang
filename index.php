4672779729796dc042381d714766565803c91887
ngocvufpt <vunnph45523@fpt.edu.vn>
2024-03-14 13:15:34 +0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 <?php
-echo $_SESSION['user_id'];
-echo $_SESSION['user_name'];
-=======
-<?php
-echo $_SESSION['user_id'];
-echo $_SESSION['user_name'];
->>>>>>> 9d956397db58d41c5eebd44ccb4351d42e7e17b3
+echo "Them san pham";
